cd0803aac62a315d5fa72a2d3761ad4fa651fbc6
nonofare <adik96885@gmail.com>
2024-10-19 23:12:38 +0200

    Add array functionality

--- Dynamic_Array/DA.h
+++ Dynamic_Array/DA.h
@@ -1,76 +1,247 @@
+#include <string>
+
 namespace DA {
 
 	template <typename T>
 	class DynArr {
 		T* arr;
 		size_t size;
-		size_t maxSize = 1;
-		const unsigned char FACTOR = 2;
+		size_t capacity;
+		const double FACTOR = 2;
 
-		void expand() {
-			newSize = 2 * size;
-			T* newArr = new T[newSize;
+		bool expand() {
+			size_t newSize = capacity * FACTOR;
+			T* newArr = nullptr;
 
-			copy(arr, size, newArr, newSize);
-			remove(arr, size);
+			try {
+				newArr = new T[newSize];
+			}
+			catch (std::bad_alloc&) {
+				return false;
+			}
 
-			size = newSize;
-			arr = newArr;
-			newArr = nullptr;
-		}
+			if (copy(newArr, newSize)) {
+				remove(arr, size);
+				capacity = newSize;
+				arr = newArr;
 
-		bool copy(T* arr1, size_t arr1Size, T* arr2, size_t arr2Size) {
-			if (arr2Size >= arr1Size) {
-				for (int i = 0; i < size; i++) {
-					arr2[i] = arr1[i];
-				}
 				return true;
 			}
 			else {
+				delete[] newArr;
+
 				return false;
 			}
 		}
 
-		void decrease() {
-			newSize = 2 / size;
-			T* newArr = new T[newSize];
+		bool copy(T* extArr, size_t extSize) {
+			if (extSize < size) {
+				return false;
+			}
 
-			copy(arr, size, newArr, newSize);
-			remove(arr, size);
+			for (int i = 0; i < size; i++) {
+				extArr[i] = arr[i];
+			}
 
-			size = size / 2;
-			arr = newArr;
-			newArr = nullptr;
+			return true;
 		}
 
-		bool remove(T* arr, size_t size) {
-			if (arr != nullptr && size >= 0) {
-				if constexpr (std::is_pointer_v<T>) {
-					for (int i = 0; i < size; i++) {
-						delete arr[i];
-					}
-				}
-				delete arr;
+		bool decrease() {
+			size_t newSize = capacity / FACTOR;
+			T* newArr = nullptr;
+
+			try {
+				newArr = new T[newSize];
+			}
+			catch (std::bad_alloc&) {
+				return false;
+			}
+
+			if (copy(newArr, newSize)) {
+				remove(arr, size);
+				capacity = newSize;
+				arr = newArr;
 
 				return true;
 			}
 			else {
-				return false
+				delete[] newArr;
+
+				return false;
 			}
 		}
 
-	public:
-		DynArr(T data) {
+		bool remove(T* extArr, size_t extSize) {
+			if (extArr == nullptr) {
+				return false;
+			}
+			/*
+			if constexpr (std::is_pointer_v<T>) {
+				for (int i = 0; i < extSize; i++) {
+					delete extArr[i];
+				}
+			}
+			*/
+			delete[] arr;
 			arr = nullptr;
+
+			return true;
+		}
+
+	public:
+		DynArr() {
+			capacity = 1;
+			arr = new T[capacity];
 			size = 0;
 		}
 
-		void push(T data) {
-
+		~DynArr() {
+			remove(arr, size);
 		}
 
+		// Method purpose: Returns the number of elements in the array
+		// Arguments: None
+		// Returns: Size of the array
+		// Time complexity: Theta(1)
+		size_t get_size() const {
+			return size;
+		}
+
+		// Method purpose: Checks if the array is empty
+		// Arguments: None
+		// Returns: True if the array is empty, false otherwise
+		// Time complexity: Theta(1)
+		bool is_empty() const {
+			return size == 0;
+		}
+
+		// Method purpose: Adds a new element to the end of the array
+		// Arguments: Data(T) of a new element
+		// Returns: Void
+		// Time complexity: Theta(n) if expansion is required, Theta(1) otherwise
+		void push(T data) {
+			if (size == capacity) {
+				expand();
+			}
+			arr[size] = data;
+			size++;
+		}
+
+		// Method purpose: Removes an element at the specified index
+		// Arguments: Index of the element
+		// Returns: Void
+		// Time complexity: Theta(n)
+		void pop(size_t index = size - 1) {
+			if (size == 0 || index >= size) {
+				throw std::out_of_range("Index is out of range");
+			}
+
+			if (size == capacity / FACTOR) {
+				decrease();
+			}
+
+			for (size_t i = index; i < size - 1; i++) {
+				arr[i] = arr[i + 1];
+			}
+			size--;
+		}
+
+		// Method purpose: Get access to an element by index (read-only)
+		// Arguments: Index of a wanted element
+		// Returns: Const reference to the element
+		// Time complexity: Theta(1)
+		const T& operator[](size_t index) const {
+			if (index >= size) {
+				throw std::out_of_range("Index is out of range");
+			}
+			else {
+				return arr[index];
+			}
+		}
+
+		// Method purpose: Get access to an element by index
+		// Arguments: Index of a wanted element
+		// Returns: Reference to the element
+		// Time complexity: Theta(1)
+		T& operator[](size_t index) {
+			if (index >= size) {
+				throw std::out_of_range("Index is out of range");
+			}
+			else {
+				return arr[index];
+			}
+		}
+
+		// Method purpose: Delete all array elements
+		// Arguments: None
+		// Returns: Void
+		// Time complexity: Theta(n) if T is pointer type, Theta(1) otherwise
 		void erase() {
 			remove(arr, size);
+			size = 0;
+			capacity = 1;
+		}
+
+		// Method purpose: Get string representation of the array
+		// Arguments: Specific to_string function if needed, number of elements to show
+		// Returns: String representation of the array
+		// Time complexity: Theta(n)
+		std::string to_str(std::string(*out_to_string)(T) = nullptr, size_t element_count = 0) {
+			std::string text;
+
+			if (element_count <= 0 || element_count > size) {
+				element_count = size;
+			}
+
+			text = "Array has " + std::to_string(int(size)) + " elements:\n";
+			if (out_to_string != nullptr) {
+				for (int i = 0; i < element_count; i++) {
+					text += out_to_string(arr[i]);
+				}
+			}
+			else if constexpr (std::is_arithmetic_v<T>) {
+				for (int i = 0; i < element_count; i++) {
+					text += std::to_string(arr[i]);
+				}
+			}
+			else {
+				text = "Data type is not supported and no method was provided";
+			}
+
+			if (element_count < size) {
+				text += "[...]\n";
+			}
+
+			return text;
+		}
+
+		// Method purpose: Sort array elements
+		// Arguments: Custom comparator if needed (< operation is default)
+		// Returns: Void
+		// Time complexity: O(n^2)
+		void sort(bool(*cmp)(T, T)) {
+			if (cmp != nullptr) {
+				for (int i = 0; i < size - 1; i++) {
+					for (int j = 0; j < size - i - 1; j++) {
+						if (cmp(arr[j], arr[j + 1])) {
+							T temp = arr[j];
+							arr[j] = arr[j + 1];
+							arr[j + 1] = temp;
+						}
+					}
+				}
+			}
+			else {
+				for (int i = 0; i < size - 1; i++) {
+					for (int j = 0; j < size - i - 1; j++) {
+						if (arr[j] > arr[j + 1]) {
+							T temp = arr[j];
+							arr[j] = arr[j + 1];
+							arr[j + 1] = temp;
+						}
+					}
+				}
+			}
 		}
 	};
 }
--- Dynamic_Array/Dynamic_Array.cpp
+++ Dynamic_Array/Dynamic_Array.cpp
@@ -1,10 +1,90 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <time.h>
 #include "DA.h"
 
 using namespace std;
 using namespace DA;
 
+struct some_object {
+	int field_1;
+	char field_2;
+};
+
+std::string some_objects_str(some_object* so) {
+	std::string text = "(" + std::to_string(so->field_1) + ", " + so->field_2 + ")\n";
+	return text;
+}
+
+bool some_objects_cmp1(some_object* so1, some_object* so2) {
+	return so1->field_1 == so2->field_1;
+}
+
+bool some_objects_cmp2(some_object* so1, some_object* so2) {
+	return so1->field_1 >= so2->field_1;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+	const int MAX_ORDER = 7;
+	const int n = pow(10, MAX_ORDER);
+	
+	DynArr <some_object*>* da = new DynArr <some_object*>();
+
+	// Pushing
+	cout << "Starting the element pushing phase" << endl;
+	double max_time_per_element = 0.0;
+	clock_t t1 = clock();
+	for (int i = 0; i < n; i++) {
+		some_object* so = new some_object();
+		so->field_1 = rand() % 100;
+		so->field_2 = 'A' + rand() % 26;
+
+		clock_t t2_element_start = clock();
+		da->push(so);
+		clock_t t2_element_end = clock();
+
+		double time_per_element = double(t2_element_end - t2_element_start) / CLOCKS_PER_SEC;
+		if (time_per_element > max_time_per_element) {
+			max_time_per_element = time_per_element;
+			cout << "New worst push time: " << max_time_per_element << "s, at index: " << i << endl;
+		}
+	}
+	clock_t t2 = clock();
+	cout << "Element pushing phase done" << endl;
+
+	double pushing_time = double(t2 - t1) / CLOCKS_PER_SEC;
+	double amortized_time = pushing_time / n;
+	cout << da->to_str(some_objects_str, 10);
+	cout << "Average time for element (amortized): " << amortized_time << "s" << endl;
+	cout << "Pushing time: " << pushing_time << "s" << endl << endl;
+
+	// Sorting
+	cout << "Starting the element sorting phase" << endl;
+	clock_t t3 = clock();
+	da->sort(some_objects_cmp2);
+	clock_t t4 = clock();
+	cout << "Element sorting phase done" << endl;
+
+	cout << da->to_str(some_objects_str, 10);
+	double sorting_time = (double(t4 - t3) / CLOCKS_PER_SEC) - pushing_time;
+	cout << "Sorting time: " << sorting_time << "s" << endl << endl;
+
+	// Poping
+	cout << "Starting the element poping phase" << endl;
+	clock_t t5 = clock();
+	da->erase();
+	clock_t t6 = clock();
+	cout << "Element poping phase done" << endl;
+
+	cout << da->to_str(some_objects_str, 10);
+	double poping_time = (double(t6 - t5) / CLOCKS_PER_SEC) - (pushing_time + sorting_time);
+	cout << "Poping time: " << sorting_time << "s" << endl << endl;
+
+	cout << "Total time: " << pushing_time + sorting_time + sorting_time << "s";
+
+	delete da;
+	return 0;
 }
